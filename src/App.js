98e82cc69c0c0c9143870493a51d8fabee4b901d
Sean Shadle <seandshadle@gmail.com>
2020-09-09 17:35:09 -0500

    Photo, title, explanation and copyright loading to the page. Starting on css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios'
+import {API_KEY, BASE_URL} from "./index"
+import Photo from "./Photo"
 
-function App() {
+
+export default function App() {
+  const [photoURL, setPhotoURL] = useState('')
+  const [copyright, setCopyright] = useState('')
+  const [title, setTitle] = useState(null)
+  const [explanation, setExplanation] = useState(null)
+
+  useEffect(() => {
+    axios
+    .get(`${BASE_URL}?api_key=${API_KEY}`)
+    .then((res) => {
+      setPhotoURL(res.data.url)
+      setCopyright(res.data.copyright)
+      setTitle(res.data.title)
+      setExplanation(res.data.explanation)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    }, [])
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className = "title-container">
+        <h1>NASA photo of the day</h1>
+        <h2>{title}</h2>
+      </div>
+      <div className="photo-container">
+        <Photo url={photoURL}/>
+      </div>
+      <div className = "info-container">
+        <p>{explanation}</p>
+        <p><b>Copyright:</b> {copyright}</p>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+export const BASE_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'ckOdodKqt8iSwhd4i1CmGtfeavuUd2LIpPymbjSy'
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
